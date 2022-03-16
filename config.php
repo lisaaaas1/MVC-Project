43449fe47a6921ffe31be2a51d7d40b7c56cb351
lisaaaas1 <el.petryaeva@mail.ru>
2022-03-16 11:37:04 +0500

    version 1.16.03

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-const DB_HOSTNAME = '89.108.83.110';
-const DB_USERNAME = 'is342_eremin';
-const DB_PASSWORD = 'testingmysql';
-const DB_DATABASE = 'is342_eremin';
+const DB_HOSTNAME = 'mysqlis.radiotech.su';
+const DB_USERNAME = 'is342_18';
+const DB_PASSWORD = 'VRC9Pq6wk4';
+const DB_DATABASE = 'is342_18_ped';
 const DB_PORT = '3306'; # Default 3306
--- views/register.php
+++ views/register.php
@@ -1,8 +1,6 @@
 <?php
     $title = 'Регистрация в системе';
-    $css = [
-        'register/main.css'
-    ];
+    $css[] = 'register/main.css';
     @include_once 'header.php';
 ?>
 
@@ -10,12 +8,25 @@
     ? "<div class='error'>Пароли не совпали!</div>"
     : "" )
 ?>
-<form action="" method="POST">
-    <input type="text" name="name" placeholder="Фамилия Имя Отчество" required>
-    <input type="text" name="login" placeholder="Укажите логин:" required>
-    <input type="password" name="passsword" placeholder="Укажите пароль:" required>
-    <input type="password" name="passsword_confirmed" placeholder="Укажите пароль повторно:" required>
-    <input type="submit" value="Регистрация">
-</form>
+    <form action="" method="POST">
+        <div class="mb-3">
+            <label for="inputName" class="form-label" >Ваше ФИО:</label>
+            <input type="text" name="name" class="form-control" id="inputName" placeholder="Фамилия Имя Отчество" required >
+        </div>
+        <div class="mb-3">
+            <label for="inputLogin" class="form-label" >Ваш логин:</label>
+            <input type="text" name="login" class="form-control" id="inputLogin" placeholder="Укажите логин" required >
+        </div>
+        <div class="mb-3">
+            <label for="inputPassword" class="form-label" >Укажите пароль:</label>
+            <input type="password" name="passsword" class="form-control" id="inputPassword" placeholder="Укажите пароль" required >
+        </div>
+        <div class="mb-3">
+            <label for="inputPasswordConfirmed" class="form-label" >Укажите пароль повторно:</label>
+            <input type="password" name="passsword_confirmed" class="form-control" id="inputPasswordConfirmed" placeholder="Укажите пароль повторно" required >
+        </div>
+        <button type="submit" class="btn btn-primary">Регистрация</button>
+    </form>
+
 
 <?php @include_once 'footer.php'; ?>
