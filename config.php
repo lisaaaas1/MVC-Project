5f8c672b946638acf2d367f8360b2535b74059be
lisaaaas1 <el.petryaeva@mail.ru>
2022-03-17 13:56:30 +0500

    version 1.17-03

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOSTNAME = 'mysqlis.radiotech.su';
-const DB_USERNAME = 'is342_18';
+const DB_USERNAME = 'is342_18_1';
 const DB_PASSWORD = 'VRC9Pq6wk4';
-const DB_DATABASE = 'is342_18_ped';
+const DB_DATABASE = 'is342_18_is342_18_p';
 const DB_PORT = '3306'; # Default 3306
--- core/view.php
+++ core/view.php
@@ -1,6 +1,8 @@
 <?php
 function view($name, $arg = []) {
-    ob_start();
+    ob_start(function ($buffer) use ($arg) {
+
+    });
     include_once 'views/' . $name . '.php';
     $content = ob_get_contents();
     ob_clean();
