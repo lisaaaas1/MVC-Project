cf6964399b826e3becc3e6d6483be7d820a08c78
lisaaaas1 <el.petryaeva@mail.ru>
2022-03-22 13:06:50 +0500

    version 1.22-03

--- views/header.php
+++ views/header.php
@@ -7,40 +7,44 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= (isset($title) ? $title : 'Главная страница') ?></title>
     <?php
-    $css[] = 'bootstrap.css';
-    $javascript[] = ['src'=>'bootstrap.js'];
+        $css[] = 'bootstrap.css';
+        $javascript[] = ['src' => 'bootstrap.js'];
+
         if(isset($css))
             foreach($css as $item)
                 echo "<link rel='stylesheet' href='/assets/css/{$item}'>";
         if(isset($javascript))
             foreach($javascript as $item)
-                echo "<script src='/assets/js/{$item['src']}' " . ($item['defer'] ? 'defer' : '' ) . "></script>"
+                echo "<script src='/assets/js/{$item['src']}' " . (isset($item['defer']) ? 'defer' : '' ) . "></script>"
     ?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Мой блог</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Мой блог</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Главная</a>
+                </li>
+                <?php if(has_session('id')): ?>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Главная</a>
+                        <a class="nav-link" href="/account">Мой аккаунт</a>
                     </li>
+                <?php else: ?>
                     <li class="nav-item">
                         <a class="nav-link" href="/login">Авторизация</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/register">Регистрация</a>
                     </li>
-
-
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
-            </div>
+                <?php endif; ?>
+            </ul>
         </div>
-    </nav>
-</head>
-<body>
+    </div>
+</nav>
